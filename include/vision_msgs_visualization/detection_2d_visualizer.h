a6d98287f6eb8ecb68633c1a8a1cdfafbb6dfb1e
Masaya Kataoka <ms.kataoka@gmail.com>
2019-05-12 16:07:42 +0900

    add vision info subscriber

--- include/vision_msgs_visualization/detection_2d_visualizer.h
+++ include/vision_msgs_visualization/detection_2d_visualizer.h
@@ -3,6 +3,7 @@
 
 // Headers in ROS
 #include <ros/ros.h>
+#include <vision_msgs/VisionInfo.h>
 #include <vision_msgs/Detection2DArray.h>
 #include <sensor_msgs/Image.h>
 #include <image_transport/image_transport.h>
@@ -13,6 +14,10 @@
 
 // Headers in STL
 #include <memory>
+#include <map>
+
+// Headers in Boost
+#include <boost/optional.hpp>
 
 class Detection2DVisualizer
 {
@@ -28,7 +33,12 @@ private:
     std::shared_ptr<message_filters::Subscriber<vision_msgs::Detection2DArray> > detection_sub_ptr_;
     std::string image_topic_;
     std::string detection_topic_;
+    std::string vision_info_topic_;
     std::shared_ptr<message_filters::TimeSynchronizer<sensor_msgs::Image, vision_msgs::Detection2DArray> > sync_ptr_;
+    image_transport::Publisher image_pub_;
+    ros::Subscriber vision_info_sub_;
+    void visionInfoCallback(const vision_msgs::VisionInfo::ConstPtr msg);
+    boost::optional<std::map<int,std::string> > classes_;
 };
 
 #endif  //VISION_MSGS_VISUALIZATION_DETECTION_2D_VISUALIZER_H_INCLUDED
--- src/detection_2d_visualizer.cpp
+++ src/detection_2d_visualizer.cpp
@@ -5,10 +5,13 @@ Detection2DVisualizer::Detection2DVisualizer(ros::NodeHandle nh,ros::NodeHandle 
 {
     pnh.param<std::string>("image_topic", image_topic_, "image_raw");
     pnh.param<std::string>("detection_topic", detection_topic_, "detection_2d");
+    pnh.param<std::string>("vision_info_topic", vision_info_topic_, "vision_info");
+    image_pub_ = it_.advertise(image_topic_+"/annotated", 1);
     image_sub_ptr_ = std::make_shared<message_filters::Subscriber<sensor_msgs::Image> >(nh,image_topic_, 1);
     detection_sub_ptr_ = std::make_shared<message_filters::Subscriber<vision_msgs::Detection2DArray> >(nh,detection_topic_, 1);
     sync_ptr_ = std::make_shared<message_filters::TimeSynchronizer<sensor_msgs::Image, vision_msgs::Detection2DArray> >(*image_sub_ptr_,*detection_sub_ptr_,10);
     sync_ptr_->registerCallback(boost::bind(&Detection2DVisualizer::callback, this, _1, _2));
+    vision_info_sub_ = nh.subscribe(vision_info_topic_,1,&Detection2DVisualizer::visionInfoCallback,this);
 }
 
 Detection2DVisualizer::~Detection2DVisualizer()
@@ -16,7 +19,22 @@ Detection2DVisualizer::~Detection2DVisualizer()
 
 }
 
+void Detection2DVisualizer::visionInfoCallback(const vision_msgs::VisionInfo::ConstPtr msg)
+{
+    return;
+}
+
 void Detection2DVisualizer::callback(const sensor_msgs::ImageConstPtr& image, const vision_msgs::Detection2DArrayConstPtr& detection)
 {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
     return;
 }
